70fb57fc8fed7ade90c9eaa96fed1f08cfe5d1b9
Sophie Vercoutter <webdesigner@sitify.fr>
2023-04-05 22:30:56 +0200

    Geolocalisation

--- script.js
+++ script.js
@@ -1,82 +1,111 @@
-// Dictionnaire de traduction
-const languageDict = {
-  fr: {
-    info: "Information nutritionnelle",
-    reg: "Énergie, Règlement",
-    energy: "Energie, Règlement UE N° 1169/2011 (kj/100g)",
-    proteines: "Protéines, N x 6,25 (g/100g)",
-    glucides: "Glucides (g/100g)",
-    lipides: "Lipides (g/100g)",
-    sucres: "Sucres (g/100g)",
-    acideGrasSatures: "Acide Gras saturés (g/100g)",
-    selChlorureSodium: "Sel chlorure de sodium (g/100g)",
-    etiquette: "Étiquette ",
-    liste:
-      "Eau, Protéines, Glucides, Alcool, Sels minéraux, Vitamines Oligo-éléments, Polyphénols, Acides minéraux",
-  },
-  en: {
-    info: "Nutritional Information",
-    reg: "Energy, EU Regulation No. 1169/2011 (kcal/100g)",
-    energy: "Energy, EU Regulation N° 1169/2011 (kj/100g)",
-    proteines: "Protein, N x 6.25 (g/100g)",
-    glucides: "Carbohydrates (g/100g)",
-    lipides: "Lipids (g/100g)",
-    sucres: "Sugar (g/100g)",
-    acideGrasSatures: "Saturated Fatty Acid (g/100g)",
-    selChlorureSodium: "Sodium chloride salt (g/100g)",
-    etiquette: "Label ",
-    liste:
-      "Water, Proteins, Carbohydrates, Alcohol, Mineral salts, Vitamins Trace elements, Polyphenols, Mineral acids",
-  },
-  es: {
-    info: "Información nutricional",
-    reg: "Energía, Reglamento UE N° 1169/2011 (kcal/100g)",
-    energy: "Energía, Reglamento UE No. 1169/2011 (kj/100g)",
-    proteines: "Proteína, N x 6.25 (g/100g)",
-    glucides: "Glúcidos (g/100g)",
-    lipides: "Lípidos (g/100g)",
-    sucres: "azúcar (g/100g)",
-    acideGrasSatures: "Ácido Graso Saturado  (g/100g)",
-    selChlorureSodium: "Sal de cloruro de sodio (g/100g)",
-    etiquette: "etiqueta ",
-    liste:
-      "Agua, Proteínas, Carbohidratos, Alcohol, Sales minerales, Vitaminas Oligoelementos, Polifenoles, Ácidos minerales",
-  },
+//TROUVER LA GEOLOCALISATION
 
-  // Ajouter des clés de traduction pour d'autres langues si nécessaire
-};
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
 
-// Récupération de la langue du navigateur
-const browserLang = navigator.language.substring(0, 2);
+    var xhr = new XMLHttpRequest();
+    var url =
+      "https://nominatim.openstreetmap.org/reverse?format=json&lat=" +
+      latitude +
+      "&lon=" +
+      longitude;
+    xhr.open("GET", url, true);
+    xhr.onload = function () {
+      var result = JSON.parse(xhr.responseText);
+      if (result.address && result.address.country_code === "us") {
+        window.location.href = "https://www.test.html";
+      } else {
+        // DICTIONNAIRE POUR LA TRADUCTION
+        const languageDict = {
+          fr: {
+            info: "Information nutritionnelle",
+            reg: "Énergie, Règlement",
+            energy: "Energie, Règlement UE N° 1169/2011 (kj/100g)",
+            proteines: "Protéines, N x 6,25 (g/100g)",
+            glucides: "Glucides (g/100g)",
+            lipides: "Lipides (g/100g)",
+            sucres: "Sucres (g/100g)",
+            acideGrasSatures: "Acide Gras saturés (g/100g)",
+            selChlorureSodium: "Sel chlorure de sodium (g/100g)",
+            etiquette: "Étiquette ",
+            liste:
+              "Eau, Protéines, Glucides, Alcool, Sels minéraux, Vitamines Oligo-éléments, Polyphénols, Acides minéraux",
+          },
+          en: {
+            info: "Nutritional Information",
+            reg: "Energy, EU Regulation No. 1169/2011 (kcal/100g)",
+            energy: "Energy, EU Regulation N° 1169/2011 (kj/100g)",
+            proteines: "Protein, N x 6.25 (g/100g)",
+            glucides: "Carbohydrates (g/100g)",
+            lipides: "Lipids (g/100g)",
+            sucres: "Sugar (g/100g)",
+            acideGrasSatures: "Saturated Fatty Acid (g/100g)",
+            selChlorureSodium: "Sodium chloride salt (g/100g)",
+            etiquette: "Label ",
+            liste:
+              "Water, Proteins, Carbohydrates, Alcohol, Mineral salts, Vitamins Trace elements, Polyphenols, Mineral acids",
+          },
+          es: {
+            info: "Información nutricional",
+            reg: "Energía, Reglamento UE N° 1169/2011 (kcal/100g)",
+            energy: "Energía, Reglamento UE No. 1169/2011 (kj/100g)",
+            proteines: "Proteína, N x 6.25 (g/100g)",
+            glucides: "Glúcidos (g/100g)",
+            lipides: "Lípidos (g/100g)",
+            sucres: "azúcar (g/100g)",
+            acideGrasSatures: "Ácido Graso Saturado  (g/100g)",
+            selChlorureSodium: "Sal de cloruro de sodio (g/100g)",
+            etiquette: "etiqueta ",
+            liste:
+              "Agua, Proteínas, Carbohidratos, Alcohol, Sales minerales, Vitaminas Oligoelementos, Polifenoles, Ácidos minerales",
+          },
 
-// Traduction des phrases en fonction de la langue du navigateur
-const info = document.querySelector("#text1");
-const reg = document.querySelector("#text2");
-const energy = document.querySelector("#text3");
-const proteines = document.querySelector("#text4");
-const glucides = document.querySelector("#text5");
-const lipides = document.querySelector("#text6");
-const sucres = document.querySelector("#text7");
-const acideGrasSatures = document.querySelector("#text8");
-const selChlorureSodium = document.querySelector("#text9");
-const etiquette = document.querySelector("#text10");
-const liste = document.querySelector("#text11");
+          // Ajouter la traduction pour d'autres langues si besoin
+        };
 
-if (languageDict.hasOwnProperty(browserLang)) {
-  const translations = languageDict[browserLang];
-  info.textContent = translations.info;
-  reg.textContent = translations.reg;
-  energy.textContent = translations.energy;
-  proteines.textContent = translations.proteines;
-  glucides.textContent = translations.glucides;
-  lipides.textContent = translations.lipides;
-  sucres.textContent = translations.sucres;
-  acideGrasSatures.textContent = translations.acideGrasSatures;
-  selChlorureSodium.textContent = translations.selChlorureSodium;
-  selChlorureSodium.textContent = translations.selChlorureSodium;
-  etiquette.textContent = translations.etiquette;
-  liste.textContent = translations.liste;
+        // Récupération de la langue du navigateur ( fr,en, es...)
+        const browserLang = navigator.language.substring(0, 2);
+
+        // Traduction des mots en fonction de la langue du navigateur
+        const info = document.querySelector("#text1");
+        const reg = document.querySelector("#text2");
+        const energy = document.querySelector("#text3");
+        const proteines = document.querySelector("#text4");
+        const glucides = document.querySelector("#text5");
+        const lipides = document.querySelector("#text6");
+        const sucres = document.querySelector("#text7");
+        const acideGrasSatures = document.querySelector("#text8");
+        const selChlorureSodium = document.querySelector("#text9");
+        const etiquette = document.querySelector("#text10");
+        const liste = document.querySelector("#text11");
+
+        if (languageDict.hasOwnProperty(browserLang)) {
+          const translations = languageDict[browserLang];
+          info.textContent = translations.info;
+          reg.textContent = translations.reg;
+          energy.textContent = translations.energy;
+          proteines.textContent = translations.proteines;
+          glucides.textContent = translations.glucides;
+          lipides.textContent = translations.lipides;
+          sucres.textContent = translations.sucres;
+          acideGrasSatures.textContent = translations.acideGrasSatures;
+          selChlorureSodium.textContent = translations.selChlorureSodium;
+          selChlorureSodium.textContent = translations.selChlorureSodium;
+          etiquette.textContent = translations.etiquette;
+          liste.textContent = translations.liste;
+        } else {
+          // Si la langue n'est pas supportée, nous écrivons cette phrase
+          console.log(`La langue ${browserLang} n'est pas supportée.`);
+        }
+      }
+    };
+    xhr.send();
+  });
 } else {
-  // Si la langue n'est pas supportée, on ne traduit pas le texte
-  console.log(`La langue ${browserLang} n'est pas supportée.`);
+  //La géolocalisation n'a pas été trouvée.
+  console.log(
+    "La géolocalisation n'est pas prise en charge par votre navigateur"
+  );
 }
